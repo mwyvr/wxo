34907014cfcad30baf5541639b2fa560e63a2148
Mike Watkins <python@mikewatkins.ca>
2022-01-10 06:47:53 -0800

    add a template fallback for user-supplied errors

--- wxo.go
+++ wxo.go
@@ -3,12 +3,13 @@ package wxo
 import (
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
 	"time"
 )
 
 const DefaultCacheExpiry = 15 * 60 // 15 minutes or 96 remote requests per day
-const defaultFormat = "{{.Condition}} {{printf \"%.1f\" .Temp}}{{.TempUnits}} {{.WindVane}}{{.WindDirection}} {{printf \"%.1f\" .WindSpeed}}{{.WindSpeedUnits}}"
+const defaultFormat = "{{.Alerts}} {{.Condition}} {{printf \"%.1f\" .Temp}}{{.TempUnits}} {{.WindVane}}{{.WindDirection}} {{printf \"%.1f\" .WindSpeed}}{{.WindSpeedUnits}}"
 
 // TODO NOT USING THIS YET
 type WxoConfig struct {
@@ -24,7 +25,7 @@ type WeatherClient interface {
 
 // SiteData provides a minimal, cross-provider view of weather data
 type SiteData struct {
-	Alerts         string // for now, a concatenation of alerts
+	Alerts         string // a concatenation of alerts
 	Location       string // name
 	Timestamp      time.Time
 	Timezone       string
@@ -37,7 +38,7 @@ type SiteData struct {
 	WindSpeedUnits string
 	WindGust       float64
 	WindDegree     int
-	WindDirection  string // ordinal N, S, E, W, NE, etc
+	WindDirection  string // ordinal N, S, E, W, NE, NNW etc
 	WindVane       string // arrow pointing the general direction
 	Latitude       float64
 	Longitude      float64
@@ -46,19 +47,34 @@ type SiteData struct {
 	Sunset         time.Time
 }
 
-// Execute allows callers to supply their own format (Go text template) string
-func (s *SiteData) Execute(format string) {
+// ExecuteTemplate allows callers to supply their own format (Go text template)
+// string. If format string isn't the default format, fall back and provide at
+// least some output using the defaultFormat.
+func (s *SiteData) ExecuteTemplate(format string) {
 	t, err := template.New("wxo").Parse(format)
+
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%v", err)
+		// attempt to fall back
+		if format != defaultFormat {
+			t, err = template.New("wxo").Parse("ERR! Fallback: " + defaultFormat)
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "%v", err)
+				return
+			}
+		} else {
+			return
+		}
+	}
+	buf := strings.Builder{}
+	err = t.Execute(&buf, s)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%v", err)
 	}
-	err = t.Execute(os.Stdout, s)
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "%v", err)
-	}
+	fmt.Fprint(os.Stdout, strings.TrimSpace(buf.String()))
 }
 
 // Print the gathered site data with a default output format
 func (s *SiteData) Print() {
-	s.Execute(defaultFormat)
+	s.ExecuteTemplate(defaultFormat)
 }
